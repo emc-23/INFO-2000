1eac321badc1cf4087fcb1e95855676e44102855
Evan Courville <emc79868@uga.edu>
2025-02-04 22:45:16 -0500

    Added UGAFootball.sh and organizer.sh

---
+++ GroupExercise1/UGAFootball.sh
@@ -0,0 +1,26 @@
+cd ~/Downloads/
+mkdir dawgs
+cd ~/Downloads/dawgs/
+touch  KirbySmart.txt GlennSchumann.txt MikeBobo.txt ArianSmith.txt GunnerStockton.txt ChazChambliss.txt CashJones.txt MalakiStarks.txt DanJackson.txt LondonHumphreys.txt JalonWalker.txt JulianHumpery.txt OscarDelp.txt KJBolden.txt BrandonMathis.txt
+ls
+mkdir players coaches
+mv ArianSmith.txt GunnerStockton.txt ChazChambliss.txt CashJones.txt MalakiStarks.txt DanJackson.txt LondonHumphreys.txt JalonWalker.txt JulianHumpery.txt OscarDelp.txt KJBolden.txt BrandonMathis.txt players
+mv KirbySmart.txt GlennSchumann.txt MikeBobo.txt
+mv coaches coachesAndStaff
+ls
+cd ../Downloads/players
+pwd
+ls
+cd ~/Downloads/dawgs/coachesAndStaff/
+ls
+cd ~/Downloads/dawgs/
+mkdir rivals
+cd ../dawgs/rivals
+touch florida_gators.txt georgiatech_yellowjackets.txt
+cd ~/Downloads/dawgs/
+mkdir medals
+cd ../dawgs/medals
+touch 2022.txt 2021.txt 1980.txt 1942.txt
+cd ~/Downloads/dawgs/
+mkdir allFiles
+cp -r players coachesAndStaff medals rivals /c/users/ajdj0/Downloads/dawgs/allFiles/
