925f763678d8b55ecbfcdcaab0c349009855df69
Evan Courville <emc79868@uga.edu>
2025-02-11 10:03:47 -0500

    Added GroupExercise2 folder

---
+++ Assignment1/conda/GroupExercise1/organizer.sh
@@ -0,0 +1,10 @@
+cd ~/Downloads/
+mv disorganized_structure orgranized_structure
+cd ~/Downloads/organized_structure
+mkdir entertainment recipes sports technology travel
+mv entertainment_file1.txt entertainment_file2.txt entertainment_file3.txt entertainment
+mv recipes_file1.txt recipes_file2.txt recipes_file3.txt recipes
+mv sports_file1.txt sports_file2.txt sports_file3.txt sports
+mv technology_file1.txt technology_file2.txt technology_file3.txt technology
+mv travel_file1.txt travel_file2.txt travel_file3.txt travel
+ls > output.txt
